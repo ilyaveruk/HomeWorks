64b0ec869173728ac30375ac99df422a061ec16c
ilia1 <ilia1@DESKTOP-OLBD3AS>
2021-04-09 13:17:07 +0300

    added comments

--- HW2/src/bank/Account.java
+++ HW2/src/bank/Account.java
@@ -8,21 +8,21 @@ public class Account {
 		this.name=name;
 	}
 	
-	
+	//return amount of NIS
 	public int getShekels() {
 		return shekels;
 	}
 	
-	
+	//return the account name
 	public String getName() {
 		return name;
 	}
 	
-	
+	//add "amount" to shekels
 	public void add(int amount) {
 		shekels+=amount;
 	}
-	
+	//make string as asked and return it
 	public String toString() { 
 		return String.format("%s has %d shekels",name, shekels);
 	}
