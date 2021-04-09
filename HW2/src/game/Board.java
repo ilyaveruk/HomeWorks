53a7cf5476eb077755afe73f0d6ba1d39c3c8240
ilia1 <ilia1@DESKTOP-OLBD3AS>
2021-04-09 13:24:12 +0300

    added maxLaneContaining and rayLength

--- HW2/src/game/Board.java
+++ HW2/src/game/Board.java
@@ -13,6 +13,8 @@ public class Board {
 		
 	}
 
+	//checks if the board[i][j] is empty if it does it sets player "p" and return true
+	//if it doesn't return false
 	protected boolean set(int i, int j, Player p) {
 
 		if (board[i][j] == null) {
@@ -61,15 +63,114 @@ public class Board {
 		return str;		
 	}		
 			
-	@SuppressWarnings("unused")
-	private int rayLength(int x, int y, int dx, int dy) {
+	
+	private int rayLength(int x, int y, char s1, char s2, char mark) {
 		
+		char Mark1;
+		if(this.get(x, y) == null)
+			return 0;
+		Mark1 = this.get(x, y).getMark();
+		if(s1 == 'd' && s2 == 'r')
+		{
+			if((x == n-1 || y == m-1) && (Mark1 == mark))
+					return 1;
+			else if(Mark1 == mark)
+				return 1+this.rayLength(x+1, y+1, s1, s2, mark);
+			else return 0;
+			
+		}
+		else if(s1 == 'd' && s2 == 'l')
+		{
+			if((x == n-1 || y == 0) && (Mark1 == mark))
+				return 1;
+			else if(Mark1 == mark)
+				return 1+this.rayLength(x+1, y-1, s1, s2, mark);
+			else return 0;
+			
+		}
+		else if(s1 == 'u' && s2 == 'r')
+		{
+			if((x == 0 || y == m-1) && (Mark1 == mark))
+				return 1;
+			else if(Mark1 == mark)
+				return 1+this.rayLength(x-1, y+1, s1, s2, mark);
+			else return 0;
+			
+		}
+		else if(s1 == 'u' && s2 == 'l')
+		{
+			if((x == 0 || y == 0) && (Mark1 == mark))
+				return 1;
+			else if(Mark1 == mark)
+				return 1+this.rayLength(x-1, y-1, s1, s2, mark);
+			else return 0;
+			
+		}
+		else if(s1 == 'u')
+		{
+			if((x == 0) && (Mark1 == mark))
+				return 1;
+			else if(Mark1 == mark)
+				return 1+this.rayLength(x-1, y, s1, s2, mark);
+			else return 0;
+			
+		}
+		else if(s1 == 'd')
+		{
+			if((x == n-1) && (Mark1 == mark))
+				return 1;
+			else if(Mark1 == mark)
+				return 1+this.rayLength(x+1, y, s1, s2, mark);
+			else return 0;
+			
+		}
+		else if(s2 == 'r')
+		{
+			if((y == m-1) && (Mark1 == mark))
+				return 1;
+			else if(Mark1 == mark)
+				return 1+this.rayLength(x, y+1, s1, s2, mark);
+			else return 0;
+		}
+		else if(s2 == 'l')
+		{
+			if((y == 0) && (Mark1 == mark))
+				return 1;
+			else if(Mark1 == mark)
+				return 1+this.rayLength(x, y-1, s1, s2, mark);
+			else return 0;
+		}
 		return 0;
 	}
 	
 	protected int maxLineContaining(int i, int j) {
 		
-		return 0;
+		char s1='x',s2='y',Mark1;
+		int k,l,max=0,temp;
+		if(this.get(i, j) == null)
+			return 0;
+		Mark1 = this.get(i, j).getMark();
+		for(k=0 ; k<3 ; k++)
+		{
+			for(l=0 ; l<3 ; l++)
+			{
+				if(k == 0)//down
+					s1='d';
+				if(k == 1)//up
+					s1='u';
+				if(l == 0)//left
+					s2='l';
+				if(l == 1)//right
+					s2='r';
+				temp = this.rayLength(i, j, s1, s2, Mark1);
+				if(temp > max)
+				{
+					max = temp;
+				}
+				s1='x'; s2='y';
+			}
+		}
+		return max;
 		
 	}
 	
