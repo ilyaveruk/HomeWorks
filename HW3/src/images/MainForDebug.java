fb616409216277aa951c248c038b96bad9d9aff8
ilia1 <ilia1@DESKTOP-OLBD3AS>
2021-05-03 23:45:56 +0300

    Mix class finish

--- HW3/src/images/MainForDebug.java
+++ HW3/src/images/MainForDebug.java
@@ -4,12 +4,13 @@ public class MainForDebug {
 
 	public static void main(String[] args) {
 		
-		Image i1 = new Gradient(100, 150, RGB.RED,
-				RGB.RED);
-				Image i2 = new Gradient(200, 100, RGB.BLUE,
-				RGB.BLUE);
-				Image i = new Superpose(i1,  i2);
+		Image i1 = new Gradient(100, 150, RGB.RED, 
+				RGB.WHITE);
+				Image i2 = new Gradient(200, 100, RGB.BLUE, 
+				RGB.GREEN);
+				Image i = new Mix(i1,  i2, 0.7);
 				Displayer.display(i);
+
 
 
 		
--- HW3/src/images/Mix.java
+++ HW3/src/images/Mix.java
@@ -2,25 +2,44 @@ package images;
 
 public class Mix extends BinaryImageDecorator{
 
+	private Image base1, base2;
+	private double alpha;
 	
-	
+	public Mix(Image base1, Image base2, double alpha) {
+	this.alpha=alpha;	
+	this.base1=base1;
+	this.base2=base2;
+	}
 	
 	@Override
 	public int getWidth() {
 		
-		return 0;
+		return base1.getWidth() > base2.getWidth() ? base1.getWidth() : base2.getWidth();
 	}
 
 	@Override
 	public int getHeight() {
 		
-		return 0;
+		return base1.getHeight() > base2.getHeight() ? base1.getHeight() : base2.getHeight();
 	}
 
 	@Override
 	public RGB get(int x, int y) {
 		
-		return null;
+		if (x > getWidth() || y > getHeight())
+			return null;
+		
+			if ((x > base1.getWidth() || y > base1.getHeight()) && (x > base2.getWidth() || y > base2.getHeight()))
+				return RGB.BLACK;
+
+			else if (x > base1.getWidth() || y > base1.getHeight())
+				return base2.get(x, y);
+
+			else if (x > base2.getWidth() || y > base2.getHeight())
+				return base1.get(x, y);
+			
+			else
+				return RGB.mix(base1.get(x, y), base2.get(x, y), alpha);
 	}
 
 }
