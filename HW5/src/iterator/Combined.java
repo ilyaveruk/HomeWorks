d1cb9f8103c7c96b5bc153a5e2dfda3c8a2ac681
ilia1 <ilia1@DESKTOP-OLBD3AS>
2021-05-30 00:09:07 +0300

    done

--- HW5/src/iterator/Combined.java
+++ HW5/src/iterator/Combined.java
@@ -1,24 +1,42 @@
 package iterator;
+
 import java.util.Iterator;
 
 public class Combined<E> implements Iterable<E> {
-	
-	
-	private Iterable<E> first , second;
-	
+
+	private Iterator<E> first;
+	private Iterator<E> second;
+	private int reset = 0;
+
 	public Combined(Iterable<E> first, Iterable<E> second) {
-		this.first=first;
-		this.second=second;
+		this.first = first.iterator();
+		this.second = second.iterator();
 	}
 
 	@Override
 	public Iterator<E> iterator() {
-		
-		return null;
+		return new CombinedIterator();
 	}
-	
-	
-	
-	
+
+	public class CombinedIterator implements Iterator<E> {
+
+		@Override
+		public boolean hasNext() {
+			return (first.hasNext() || second.hasNext());
+		}
+
+		@Override
+		public E next() {
+			if ((reset == 0 && first.hasNext() && second.hasNext())) {
+				reset++;
+				return first.next();
+			} else if (reset == 1 && first.hasNext() && second.hasNext()) {
+				reset = 0;
+				return second.next();
+			} else if (!(first.hasNext()))
+				return second.next();
+			return first.next();
+		}
+	}
 
 }
--- HW5/src/iterator/TwoArrays.java
+++ HW5/src/iterator/TwoArrays.java
@@ -19,7 +19,8 @@ public class TwoArrays implements Iterable<Integer> {
 
 	private class TwoArrayIterator implements Iterator<Integer> {
 		private int count = 0;
-		private int i = 0, j = 0, holder = 0;
+		private int i = 0, j = 0;
+		private Integer holder = 0;
 
 		@Override
 		public boolean hasNext() {
@@ -32,22 +33,6 @@ public class TwoArrays implements Iterable<Integer> {
 			int maxTemp[] = a1.length > a2.length ? a1 : a2;
 			int minTemp[] = a1.length <= a2.length ? a1 : a2;
 			int totalLength = maxTemp.length + minTemp.length;
-//			int i = 0, j = 1, cnt = 0;
-//			Integer[] a = new Integer[a1.length + a2.length];
-//
-//			for (; cnt < min; i = i + 2, j = j + 2, cnt++) {
-//				a[i] = a1[cnt];
-//				a[j] = a2[cnt];
-//			}
-//
-//			for (j--; j < a.length; j++, cnt++)
-//				if (a2.length == max)
-//					a[j] = a2[cnt];
-//				else
-//					a[j] = a1[cnt];
-//
-//			return a[count++];
-//-----------------------------------------------------------------
 
 			if (a2.length >= a1.length) {
 				for (; count < totalLength; count++) {
@@ -69,10 +54,8 @@ public class TwoArrays implements Iterable<Integer> {
 					}
 
 				}
-					
 
-			}
-			else {
+			} else {
 				for (; count < totalLength; count++) {
 
 					if (count % 2 == 0 && i < maxTemp.length) {
@@ -80,32 +63,22 @@ public class TwoArrays implements Iterable<Integer> {
 						holder = maxTemp[i++];
 						return holder;
 					}
-					if (count % 2 != 0 && j < minTemp.length ) {
+					if (count % 2 != 0 && j < minTemp.length) {
 						count++;
 						holder = minTemp[j++];
 						return holder;
 					}
 
-					if (j == minTemp.length && i <= maxTemp.length-2) {
+					if (j == minTemp.length && i <= maxTemp.length - 2) {
 						holder = maxTemp[i++];
 						return holder;
 					}
 
 				}
-	
+
 			}
-			return maxTemp[maxTemp.length -1];
+			return maxTemp[maxTemp.length - 1];
 		}
-
-	}
-
-	public static void main(String[] args) {
-		int[] a1 = { 1, 2, 3, 4 };
-		int[] a2 = { 100 ,102,103,104,105,106};
-
-		TwoArrays aa = new TwoArrays(a1, a2);
-		for (int i : aa)
-			System.out.print(i + " ");
 
 	}
 
